f64b7555d998749b53e2088f83167c19ead97a20
EvaTu <evatuw@gmail.com>
2022-04-14 18:09:15 -0400

    before styling

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
+import {useState, useEffect} from 'react'
 import './App.css';
 
 function App() {
+  const [ items, setItems] = useState([])
+  const [ visible, setVisible] = useState(6)
+
+  useEffect(()=> {
+    fetch("https://jsonplaceholder.typicode.com/posts")
+    .then(resp => resp.json())
+    .then (data => setItems(() => data))
+
+  },[])
+
+  function handleLoadMore(){
+    setVisible(()=> visible + 3)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="card-list">
+        {items.slice(0,visible).map((item, index) =>
+        <div className="card">
+          <span>{index + 1}</span>
+          <p>{item.body}</p>
+        </div>
+          )}
+        <button onClick={handleLoadMore}>
+          Load More
+        </button>
+      </div>
+      
     </div>
   );
 }
